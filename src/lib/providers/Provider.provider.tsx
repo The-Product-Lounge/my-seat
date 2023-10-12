138ed1df071eec1d937e72140a4f98df545b0a71
AlmogH <almoghagbi96@gmail.com>
2023-10-12 18:44:15 +0300

    adding material ui

---
+++ src/lib/providers/Provider.provider.tsx
@@ -0,0 +1,10 @@
+import ThemeRegistry from "./theme/MuiThemeRegistry.provider"
+
+export default function Provider(props: { children: React.ReactNode })  {
+    const { children } = props;
+    return(
+        <ThemeRegistry options={{ key: 'mui' }}>
+            {children}
+        </ThemeRegistry>
+    )
+}
