3d75dea778a7ae8eafe4fef66c47071f544f8ef5
AlmogH <almoghagbi96@gmail.com>
2023-11-16 15:54:17 +0200

    Add new dependencies and custom viewports for Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,8 +2,21 @@ import type { Preview } from "@storybook/react";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "../src/lib/providers/theme/Theme.theme";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 import "@/app/globals.css";
+
+const customViewports = {
+  iphone14promax: {
+    name: "iPhone 14 Pro Max",
+    styles: {
+      height: "852px",
+      width: "393px",
+    },
+    type: "mobile",
+  },
+  ...INITIAL_VIEWPORTS,
+};
 
 export const decorators = [
   withThemeFromJSXProvider({
@@ -20,6 +33,10 @@ const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     expanded: true, // Adds the description and default columns
+    viewport: {
+      viewports: customViewports,
+      defaultViewport: "iphone14promax",
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
