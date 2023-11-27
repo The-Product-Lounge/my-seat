22f969e2107deffd881364620ef8945c13f88df6
AlmogH <almoghagbi96@gmail.com>
2023-11-27 18:09:17 +0200

    Add EmptyEventsText component and its story

---
+++ src/app/event-settings/EmptyEventsText.component.tsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+
+export const EmptyEventsText = () => {
+  return (
+    <Typography variant="poppins" color="#777781" textAlign="center">
+      No upcoming events yet.
+      <br /> Click ‘Create Event’ to get started!
+    </Typography>
+  );
+};
