91853d1fac78ff3dead71858a9e20a38b87f5701
AlmogH <almoghagbi96@gmail.com>
2023-11-28 21:18:57 +0200

    Add EmptyEvents component to event settings page

---
+++ src/app/event-settings/EmptyEvents.component.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/lib/components/inputs/Button.component";
+import EmptyEventsImg from "@/../public//images/events/EmptyEvents.png";
+import CreateEventImg from "@/../public/images/events/create event.svg";
+import { EmptyEventsText } from "./EmptyEventsText.component";
+
+export const EmptyEvents = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "32px",
+        marginTop: "64px",
+      }}
+    >
+      <Image src={EmptyEventsImg} width="243" alt="Empty events" />
+      <EmptyEventsText />
+      <Link href="/event-settings/create-event">
+        <Button
+          color="secondary"
+          startIcon={<Image src={CreateEventImg} alt="Create Event" />}
+        >
+          Create Event
+        </Button>
+      </Link>
+    </div>
+  );
+};
--- src/app/event-settings/page.tsx
+++ src/app/event-settings/page.tsx
@@ -1,38 +1,12 @@
-import Image from "next/image";
 import { Toolbar } from "./Toolbar.component";
-
-import EmptyEvents from "@/../public//images/events/EmptyEvents.png";
-import { EmptyEventsText } from "./EmptyEventsText.component";
-import { Button } from "@/lib/components/inputs/Button.component";
-import CreateEventImg from "@/../public/images/events/create event.svg";
-import Link from "next/link";
+import { EmptyEvents } from "./EmptyEvents.component";
 
 export default function Page() {
   return (
     <>
       <Toolbar />
       {/* Empty events */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          gap: "32px",
-          marginTop: "64px",
-        }}
-      >
-        <Image src={EmptyEvents} width="243" alt="Empty events" />
-        <EmptyEventsText />
-        <Link href="/event-settings/create-event">
-          <Button
-            color="secondary"
-            startIcon={<Image src={CreateEventImg} alt="Create Event" />}
-          >
-            Create Event
-          </Button>
-        </Link>
-      </div>
+      <EmptyEvents />
     </>
   );
 }
