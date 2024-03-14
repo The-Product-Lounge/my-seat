122b03f0319000189112b410e4f6c652fa2d2297
AlmogH <almoghagbi96@gmail.com>
2024-03-14 12:30:01 +0200

    Remove unitests

--- src/stories/components/events/EventBackground.stories.tsx
+++ src/stories/components/events/EventBackground.stories.tsx
@@ -1,6 +1,4 @@
-import type { Meta } from "@storybook/react";
-import type { StoryObjWithPlay } from "@/play-function";
-import { expect, within } from "@storybook/test";
+import type { Meta, StoryObj } from "@storybook/react";
 import { EventBackground } from "@/lib/components/events/EventBackground";
 
 const meta = {
@@ -10,30 +8,5 @@ const meta = {
 } as Meta<typeof EventBackground>;
 
 export default meta;
-type StoryWithPlay = StoryObjWithPlay<typeof meta>;
-
-export const EventBackgroundExample: StoryWithPlay = {
-	play: async ({ args, canvasElement }) => {
-		const canvas = within(canvasElement);
-		expect(canvas).toBeInTheDocument();
-		const backgroundImage = canvas.queryByRole("img") as HTMLImageElement;
-		expect(decodeURIComponent(backgroundImage.src)).toContain(
-			args.url as string,
-		);
-	},
-	args: {
-		url: "https://example.com/image.jpg",
-	},
-};
-
-export const EventBackgroundExampleWithNoImage: StoryWithPlay = {
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		expect(canvas).toBeInTheDocument();
-		const backgroundImage = canvas.queryByRole("img") as HTMLImageElement;
-		expect(backgroundImage).not.toBeInTheDocument();
-	},
-	args: {
-		url: "",
-	},
-};
+type Story = StoryObj<typeof EventBackground>;
+export const EventBackgroundExample: Story = {};
