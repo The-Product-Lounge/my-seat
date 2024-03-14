8837e22e1ac9d61226cddad002537ad1ad6016a6
AlmogH <almoghagbi96@gmail.com>
2024-03-14 12:36:46 +0200

    Refactor EventBackground component and add logo

--- src/lib/components/events/EventBackground.tsx
+++ src/lib/components/events/EventBackground.tsx
@@ -1,20 +1,27 @@
 // Imports
 import Card from "@mui/material/Card";
-
-/**
- * Interface for the EventBackground component,
- * have an imageURL prop as a string
- */
-interface EventBackgroundProps {
-	/**
-	 * The url of the background image
-	 */
-	url: string;
-}
+import bgImage from "@/../public/images/wave background.svg";
+import Image from "next/image";
+import logo from "@/../public/images/logo/logo white.svg";
 
 /**
  * EventBackground component
  */
-export const EventBackground = ({ url }: EventBackgroundProps) => (
-	<Card>Example {url}</Card>
+export const EventBackground = () => (
+	<Card className="rounded-none shadow">
+		<div className="relative z-10 flex h-32 justify-between overflow-hidden p-5">
+			<Image src={logo} className="z-30 " alt="Logo"></Image>
+			<Image
+				src={bgImage}
+				className="absolute left-0 top-0 z-20 -translate-x-1/2 scale-x-90 opacity-20"
+				alt="Lines for Background Image"
+			></Image>
+			<div
+				className="absolute left-0 top-0 h-full w-full"
+				style={{
+					backgroundColor: "#282828",
+				}}
+			></div>
+		</div>
+	</Card>
 );
